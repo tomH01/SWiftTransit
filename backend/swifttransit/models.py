07660f66ff6bb1fd28008a14b59ad8184c0ac536
Tom Hagel <tomth.hagel@gmail.com>
2024-11-09 15:31:07 +0100

    simplify box

--- backend/swifttransit/models.py
+++ backend/swifttransit/models.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from django.db.models import IntegerField, BooleanField
+from django.db.models import IntegerField, BooleanField, ForeignKey
 from django.db.models.fields import CharField
 
 
@@ -13,13 +13,15 @@ class OccupancyChoices(models.TextChoices):
     VERY_HIGH = ('VH', 'Very High')
 
 
-class BoxPositions(models.TextChoices):
-    MIDDLE = ('M', 'Middle')
-    BACK = ('B', 'Back')
+class BusLine(models.Model):
+    line_number = IntegerField(null=True, blank=False, default=-1)
+    direction = CharField(max_length=64)
 
 
 class BaseFeedback(models.Model):
-    bus_line = IntegerField(null=True, blank=False, default=-1)
+    bus_line = ForeignKey(BusLine,
+                          related_name='+',
+                          on_delete=models.CASCADE)
     time_stamp = models.DateTimeField(null=True, blank=False, default=-1)
 
     class Meta:
@@ -35,37 +37,27 @@ class Occupancy(BaseFeedback):
 
 
 class Box(BaseFeedback):
-    box_positions = CharField(null=False,
-                              blank=False,
-                              choices=BoxPositions,
-                              default='MIDDLE',
-                              max_length=64)
     bike_count = IntegerField(null=False,
                               blank=False,
                               default=1,
                               validators=[MinValueValidator(0), MaxValueValidator(4)])
-    pram_count = IntegerField(null=False,
-                              blank=False,
-                              default=0,
-                              validators=[MinValueValidator(0), MaxValueValidator(4)])
-    wheel_chair_count = IntegerField(null=False,
-                              blank=False,
-                              default=0,
-                              validators=[MinValueValidator(0), MaxValueValidator(4)])
+    wheelchair_or_pram = BooleanField(null=False,
+                                      blank=False,
+                                      default=False)
 
     def clean(self):
-        if (self.pram_count or self.wheel_chair_count) and self.bike_count:
+        if self.wheelchair_or_pram and self.bike_count:
             raise ValidationError("Bike is not allowed with wheelchair or pram.")
 
 
 
 class Changeover(BaseFeedback):
-    to_line = IntegerField(null=False, blank=False)
+    to_line = ForeignKey(BusLine,
+                         related_name='+',
+                         on_delete=models.CASCADE)
     changeover_managed = BooleanField(null=False, blank=False, default=True)
 
 
 class OnTime(BaseFeedback):
     on_time = BooleanField(default=True)
-
-
 
