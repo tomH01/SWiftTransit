0e860772f9137bf77f71ea1ba747baa673dc0224
sgrieble <sophie.grieble@gmail.com>
2024-11-10 11:12:13 +0100

    update routes endpoint for Boris and Christiane

--- backend/swifttransit/views.py
+++ backend/swifttransit/views.py
@@ -61,14 +61,47 @@ class RoutesBoris(APIView):
                 {
                     'name': 10,
                     'type': 'bus',
+                    'departure': '6:44',
+                    'arrival': '6:52',
+                    'departure_delay': 1,
+                    'arrival_delay': 2,
+                    'departure_actual': '6:45',
+                    'arrival_actual': '6:53',
+                    'bicycle': True,
+                    'occupancy': 1,
+                    'departure_stop': 'Österbergstraße',
+                    'arrival_stop': 'Hauptbahnhof'
+
+                },
+                {
+                    'name': 8,
+                    'type': 'bus',
+                    'departure': '7:05',
+                    'arrival': '7:22',
+                    'departure_delay': 2,
+                    'arrival_delay': 1,
+                    'departure_actual': '7:07',
+                    'arrival_actual': '7:23',
+                    'bicycle': False,
+                    'occupancy': 2,
+                    'departure_stop': 'Hauptbahnhof',
+                    'arrival_stop': 'Hagelloch Rathhaus'
+                }
+            ],
+            [
+                {
+                    'name': 10,
+                    'type': 'bus',
                     'departure': '7:21',
                     'arrival': '7:25',
                     'departure_delay': 3,
                     'arrival_delay': 4,
-                    'departure_actual': '7:21',
-                    'arrival_actual': '7:25',
-                    'bicycle': True,
-                    'occupancy': 1
+                    'departure_actual': '7:24',
+                    'arrival_actual': '7:29',
+                    'bicycle': False,
+                    'occupancy': 1,
+                    'departure_stop': 'Österbergstraße',
+                    'arrival_stop': 'Hauptbahnhof'
                 },
                 {
                     'name': 8,
@@ -77,10 +110,12 @@ class RoutesBoris(APIView):
                     'arrival': '7:52',
                     'departure_delay': 2,
                     'arrival_delay': 1,
-                    'departure_actual': '7:21',
-                    'arrival_actual': '7:25',
-                    'bicycle': True,
-                    'occupancy': 1
+                    'departure_actual': '7:34',
+                    'arrival_actual': '7:53',
+                    'bicycle': False,
+                    'occupancy': 2,
+                    'departure_stop': 'Hauptbahnhof',
+                    'arrival_stop': 'Hagelloch Rathhaus'
                 }
             ],
             [
@@ -91,22 +126,26 @@ class RoutesBoris(APIView):
                     'arrival': '7:55',
                     'departure_delay': 2,
                     'arrival_delay': 2,
-                    'departure_actual': '7:21',
-                    'arrival_actual': '7:25',
+                    'departure_actual': '7:52',
+                    'arrival_actual': '7:57',
                     'bicycle': True,
-                    'occupancy': 1
+                    'occupancy': 1,
+                    'departure_stop': 'Österbergstraße',
+                    'arrival_stop': 'Hauptbahnhof'
                 },
                 {
                     'name': 8,
                     'type': 'bus',
-                    'departure': '7:32',
-                    'arrival': '7:52',
+                    'departure': '8:02',
+                    'arrival': '2:52',
                     'departure_delay': 3,
                     'arrival_delay': 1,
-                    'departure_actual': '7:21',
-                    'arrival_actual': '7:25',
+                    'departure_actual': '8:05',
+                    'arrival_actual': '8:23',
                     'bicycle': True,
-                    'occupancy': 1
+                    'occupancy': 1,
+                    'departure_stop': 'Hauptbahnhof',
+                    'arrival_stop': 'Hagelloch Rathhaus'
                 }
             ],
             [
@@ -117,10 +156,12 @@ class RoutesBoris(APIView):
                     'arrival': '8:25',
                     'departure_delay': 5,
                     'arrival_delay': 4,
-                    'departure_actual': '7:21',
-                    'arrival_actual': '7:25',
-                    'bicycle': True,
-                    'occupancy': 1
+                    'departure_actual': '8:26',
+                    'arrival_actual': '8:29',
+                    'bicycle': False,
+                    'occupancy': 2,
+                    'departure_stop': 'Österbergstraße',
+                    'arrival_stop': 'Hauptbahnhof'
                 },
                 {
                     'name': 8,
@@ -129,10 +170,12 @@ class RoutesBoris(APIView):
                     'arrival': '8:52',
                     'departure_delay': 3,
                     'arrival_delay': 3,
-                    'departure_actual': '7:21',
-                    'arrival_actual': '7:25',
+                    'departure_actual': '8:35',
+                    'arrival_actual': '8:55',
                     'bicycle': True,
-                    'occupancy': 1
+                    'occupancy': 1,
+                    'departure_stop': 'Hauptbahnhof',
+                    'arrival_stop': 'Hagelloch Rathhaus'
                 }
             ],
         ]
@@ -144,80 +187,66 @@ class RoutesChristiane(APIView):
         routes = [
             [
                 {
-                    'name': 10,
-                    'type': 'bus',
-                    'departure': '7:21',
-                    'arrival': '7:25',
-                    'departure_delay': 3,
-                    'arrival_delay': 4,
-                    'departure_actual': '7:21',
-                    'arrival_actual': '7:25',
-                    'bicycle': True,
-                    'occupancy': 1
-                },
-                {
                     'name': 8,
                     'type': 'bus',
-                    'departure': '7:32',
-                    'arrival': '7:52',
+                    'departure': '7:13',
+                    'arrival': '7:22',
                     'departure_delay': 2,
                     'arrival_delay': 1,
-                    'departure_actual': '7:21',
-                    'arrival_actual': '7:25',
-                    'bicycle': True,
-                    'occupancy': 1
+                    'departure_actual': '7:15',
+                    'arrival_actual': '7:23',
+                    'bicycle': False,
+                    'occupancy': 2,
+                    'departure_stop': 'Gösstraße',
+                    'arrival_stop': 'Hagelloch Rathhaus'
                 }
             ],
             [
                 {
-                    'name': 10,
-                    'type': 'bus',
-                    'departure': '7:51',
-                    'arrival': '7:55',
-                    'departure_delay': 2,
-                    'arrival_delay': 2,
-                    'departure_actual': '7:21',
-                    'arrival_actual': '7:25',
-                    'bicycle': True,
-                    'occupancy': 1
-                },
-                {
                     'name': 8,
                     'type': 'bus',
-                    'departure': '7:32',
+                    'departure': '7:40',
                     'arrival': '7:52',
-                    'departure_delay': 3,
+                    'departure_delay': 2,
                     'arrival_delay': 1,
-                    'departure_actual': '7:21',
-                    'arrival_actual': '7:25',
-                    'bicycle': True,
-                    'occupancy': 1
+                    'departure_actual': '7:42',
+                    'arrival_actual': '7:53',
+                    'bicycle': False,
+                    'occupancy': 2,
+                    'departure_stop': 'Gösstraße',
+                    'arrival_stop': 'Hagelloch Rathhaus'
                 }
             ],
             [
                 {
-                    'name': 10,
+                    'name': 8,
                     'type': 'bus',
-                    'departure': '8:21',
-                    'arrival': '8:25',
-                    'departure_delay': 5,
-                    'arrival_delay': 4,
-                    'departure_actual': '7:21',
-                    'arrival_actual': '7:25',
+                    'departure': '8:10',
+                    'arrival': '2:52',
+                    'departure_delay': 3,
+                    'arrival_delay': 1,
+                    'departure_actual': '8:13',
+                    'arrival_actual': '8:23',
                     'bicycle': True,
-                    'occupancy': 1
-                },
+                    'occupancy': 1,
+                    'departure_stop': 'Gösstraße',
+                    'arrival_stop': 'Hagelloch Rathhaus'
+                }
+            ],
+            [
                 {
                     'name': 8,
                     'type': 'bus',
-                    'departure': '8:32',
+                    'departure': '8:40',
                     'arrival': '8:52',
                     'departure_delay': 3,
                     'arrival_delay': 3,
-                    'departure_actual': '7:21',
-                    'arrival_actual': '7:25',
+                    'departure_actual': '8:43',
+                    'arrival_actual': '8:55',
                     'bicycle': True,
-                    'occupancy': 1
+                    'occupancy': 1,
+                    'departure_stop': 'Gösstraße',
+                    'arrival_stop': 'Hagelloch Rathhaus'
                 }
             ],
         ]
